af7927520ea978b7d0dc3fc5afc233dbeea14cc9
Jum <jumtsai@foxmail.com>
2019-07-27 14:39:05 +0800

    First add network.py

---
+++ network.py
@@ -0,0 +1,173 @@
+from __future__ import print_function, division
+import scipy
+
+from keras.datasets import mnist
+from keras_contrib.layers.normalization.instancenormalization import InstanceNormalization
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+import datetime
+import matplotlib.pyplot as plt
+import sys
+from data_loader import DataLoader
+import numpy as np
+import os
+
+class model():
+    def __init__(self):
+        # Input shape
+        self.img_rows = 256
+        self.img_cols = 256
+        self.channels = 1
+        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+
+        # Configure data loader
+        self.dataset_name = 'deblur'
+        self.data_loader = DataLoader(dataset_name=self.dataset_name,
+                                      img_res=(self.img_rows, self.img_cols))
+
+
+        # Calculate output shape of D (PatchGAN)
+        patch = int(self.img_rows / 2**4)
+        self.disc_patch = (patch, patch, 1)
+
+        # Number of filters in the first layer of G
+        self.gf = 32
+
+        # Loss weights
+        self.lambda_consist = 10.0                    # Consistency loss
+        self.lambda_id = 0.1 * self.lambda_consist    # Identity loss
+
+        optimizer = Adam(0.0002, 0.5)
+        #-------------------------
+        # Construct Computational
+        #   Graph of Generators
+        #-------------------------
+
+        # Build the generators
+        self.g = self.build_generator()
+
+
+        # Input images from both domains
+        img_A = Input(shape=self.img_shape)
+        img_B = Input(shape=self.img_shape)
+
+        # Translate images to the other domain
+        deblur = self.g(img_A)
+
+        img_id = self.g(img_B)
+
+        # Combined model trains generators to fool discriminators
+        self.combined = Model(inputs=[img_A, img_B],
+                              outputs=[ deblur, img_id ])
+        self.combined.compile(loss=['mse', 'mae'],
+                            loss_weights=[ self.lambda_consist, self.lambda_id ],
+                            optimizer=optimizer)
+
+    def build_generator(self):
+        """U-Net Generator"""
+
+        def conv2d(layer_input, filters, f_size=4):
+            """Layers used during downsampling"""
+            d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
+            d = LeakyReLU(alpha=0.2)(d)
+            d = InstanceNormalization()(d)
+            return d
+
+        def deconv2d(layer_input, skip_input, filters, f_size=4, dropout_rate=0):
+            """Layers used during upsampling"""
+            u = UpSampling2D(size=2)(layer_input)
+            u = Conv2D(filters, kernel_size=f_size, strides=1, padding='same', activation='relu')(u)
+            if dropout_rate:
+                u = Dropout(dropout_rate)(u)
+            u = InstanceNormalization()(u)
+            u = Concatenate()([u, skip_input])
+            return u
+
+        # Image input
+        d0 = Input(shape=self.img_shape)
+
+        # Downsampling
+        d1 = conv2d(d0, self.gf)
+        d2 = conv2d(d1, self.gf*2)
+        d3 = conv2d(d2, self.gf*4)
+        d4 = conv2d(d3, self.gf*8)
+
+        # Upsampling
+        u1 = deconv2d(d4, d3, self.gf*4)
+        u2 = deconv2d(u1, d2, self.gf*2)
+        u3 = deconv2d(u2, d1, self.gf)
+
+        u4 = UpSampling2D(size=2)(u3)
+        output_img = Conv2D(self.channels, kernel_size=4, strides=1, padding='same', activation='tanh')(u4)
+
+        return Model(d0, output_img)
+
+    def train(self, epochs, batch_size=1, sample_interval=50):
+
+        start_time = datetime.datetime.now()
+
+        # Adversarial loss ground truths
+        valid = np.ones((batch_size,) + self.disc_patch)
+        fake = np.zeros((batch_size,) + self.disc_patch)
+
+        for epoch in range(epochs):
+            for batch_i, (imgs_A, imgs_B) in enumerate(self.data_loader.load_batch(batch_size)):
+
+                deblur = self.g.predict(image_A)
+
+                # ------------------
+                #  Train Generators
+                # ------------------
+
+                # Train the generators
+                g_loss = self.combined.train_on_batch([imgs_A, imgs_B],
+                                                        [imgs_A, imgs_B])
+
+                elapsed_time = datetime.datetime.now() - start_time
+
+                # Plot the progress
+                print ("[Epoch %d/%d] [Batch %d/%d] [G loss: %05f, id: %05f] time: %s " \
+                                                                        % ( epoch, epochs,
+                                                                            batch_i, self.data_loader.n_batches,
+                                                                            g_loss[0],
+                                                                            np.mean(g_loss[1:3]),
+                                                                            elapsed_time))
+
+                # If at save interval => save generated image samples
+                if batch_i % sample_interval == 0:
+                    self.sample_images(epoch, batch_i)
+
+    def sample_images(self, epoch, batch_i):
+        os.makedirs('images/%s' % self.dataset_name, exist_ok=True)
+        r, c = 2, 3
+
+        imgs_A = self.data_loader.load_data(domain="A", batch_size=1, is_testing=True)
+        imgs_B = self.data_loader.load_data(domain="B", batch_size=1, is_testing=True)
+
+        deblur = self.g.predict(imgs_A)
+
+        gen_imgs = np.concatenate([imgs_A, deblur, imgs_B])
+
+        # Rescale images 0 - 1
+        gen_imgs = 0.5 * gen_imgs + 0.5
+
+        titles = ['Blur', 'Deblur', 'Original']
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i,j].imshow(gen_imgs[cnt])
+                axs[i, j].set_title(titles[j])
+                axs[i,j].axis('off')
+                cnt += 1
+        fig.savefig("images/%s/%d_%d.png" % (self.dataset_name, epoch, batch_i))
+        plt.close()
+
+
+if __name__ == '__main__':
+    gan = model()
+    gan.train(epochs=200, batch_size=1, sample_interval=200)
